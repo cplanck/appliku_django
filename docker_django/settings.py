10e9df6364a687a0d58729d48c494f1b276ee5a0
Cameron Planck <cameronplanck@gmail.com>
2024-02-14 12:00:25 -0500

    updates

--- docker_django/settings.py
+++ docker_django/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / '/database/db.sqlite3'
-        'NAME': '/database/db.sqlite3'
+        'NAME': 'db.sqlite3'
     }
 }
 
