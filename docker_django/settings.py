7c664d66cf115b4dc40ee3583b11eed15b1142bd
Cameron Planck <cameronplanck@gmail.com>
2024-02-14 12:05:35 -0500

    updates

--- docker_django/settings.py
+++ docker_django/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 import environ
 
 env = environ.Env()
@@ -79,7 +80,8 @@ WSGI_APPLICATION = 'docker_django.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / '/database/db.sqlite3'
+        # 'NAME': BASE_DIR / '/database/db.sqlite3'
+        'NAME': os.path.join(BASE_DIR, 'database/db.sqlite3')
     }
 }
 
